7cd242a4ec38f4dc3d1d2e60109e8d6615127c27
Laura van Nieuwland <lauravannieuwland@gmail.com>
2021-02-03 18:04:43 +0100

    Add Pages home about products contact

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+  def home
+  
+  end
+
+  def about
+  end
+
+  def products
+    # @products = Product.all
+  end
+
+  def contact
+  end
+
+  def kladblok
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # static pages for visitors
+  root to: 'pages#home'
+  get '/about' => 'pages#about'
+  get '/products' => 'pages#products'
+  get '/contact' => 'pages#contact'
+  get '/kladblok' => 'pages#kladblok'
 end
