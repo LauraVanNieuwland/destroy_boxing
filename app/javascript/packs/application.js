9cc7735fd036206e658862a9d7f93e862458a136
Laura van Nieuwland <lauravannieuwland@gmail.com>
2021-02-08 14:05:28 +0100

    Add appliction.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import { activeNavbar } from '../components/navbar'
+
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -16,6 +16,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import { activeNavbar } from '../components/navbar'
 
 document.addEventListener('turbolinks:load', () => {
   // Call your JS functions here
